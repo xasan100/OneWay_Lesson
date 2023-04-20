8593409fe0ff0fbde59872a930f4f382a752a230
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-04-20 19:59:49 +0500

    done

---
+++ g3/index.js
@@ -0,0 +1,9 @@
+// Array  Metods VA Aarrey nima nimga ishlatamiz 
+// [0] at index 
+// toString conver arreyni stringa
+// arreyni string convert join('') va farqi string bilan
+// concat(ar1) arey bir biriga malumot qo`shish uchun 
+// typeOf
+var one = [1234, '1', 'hello world', true, undefined, false, {}]
+
+console.log(one.at(-5));
