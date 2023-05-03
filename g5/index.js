8eff8cfe20bfa1e718bc1904aeea1f2944c7dff9
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-03 20:50:50 +0500

    done

---
+++ g5/index.js
@@ -0,0 +1,54 @@
+// console.log(one);
+// var one = '1'
+// console.log(one);
+
+// 1 string 2 number 3 boolean 4 undefined 5 null 6 arrey [{}]  7sybool
+
+// data type nech turga bo`linadi 2 turga 
+
+// primativ --> string number boolean undefined  null synool
+// non primativ arrey
+
+// var a = 'hello world'
+// var b =['hello world',true ,false,2134]
+
+// var a = [1]
+// var b = [1]
+// console.log(a === b);
+
+
+// aroow fuction
+
+// •	Array nima ?
+// •	Array Methodlari: index, length, json.stringify(),
+// slice, splice, join(), concat(),
+// push(), pop(), shift(), unshift()
+// •	Map, reduce, sort, filter, forEach
+
+// var one = [1,2,3,4,5]
+// console.log(one.pop(1));
+
+// let colors = ['red', 'green', 'blue'];
+// // let cmyk = ['cyan', 'magenta', 'yellow', 'back'];
+
+// colors.unshift('hey');
+
+// console.log(colors);
+
+// filter sharti qanoatlantirsa kegin ishlaydi true flase 
+// uziga callback fuction oladi callbak fuction 3 ta qiymat oladi value index arrey
+//  (()=>) callback fuction 
+// map ham uziga callback fuction oladi callback fuction ichida 3 ta qiymat value index arrey
+
+// const one = [1, 23,10, 4, 5, 6, 7]
+
+// b = one.filter((value) => {
+//     return value >= 10
+// })
+// console.log(b);
+
+
+
+
+
+
