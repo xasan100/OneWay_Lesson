b6b3cbf5686131824632751b8b2104efc5633c70
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-07 00:04:19 +0500

    done

---
+++ g4/index.js
@@ -0,0 +1,47 @@
+// hoisting holati
+// console.log(one);
+let one = 'hello'
+// console.log(one);
+
+// {
+//     let a  = 11223
+// }
+// {
+//     let b = '1'
+//     console.log(a);
+// }
+
+
+// 1 number 2 string 3 boolean 3 null 5 undifaind 6 sybool 7 object arrey
+// sybool  !@#$%^&*() 
+
+
+// data type 
+// primativ 1 number 2 string 3 boolean 3 null 5 undifaind 6 sybool
+// nan primativ arrey object
+// const a = [1,2,[1234],3,5]
+
+// let numbers = [0, 1, 2, 3, 10, 20, 30];
+// numbers.sort((a,b)=> a-b);
+// console.log(numbers);
+
+// const arr = [1343545,[343454,[23456,[21345]]]]
+// console.log(arr.flat(Infinity));
+
+// const arr = [1, 2, 3, 4, 5, 1]
+
+// b = arr.reduce((a, b) => a + b, 0)
+// console.log(b);
+
+// const sumWithInitial = array1.reduce(
+//     (accumulator, currentValue) => accumulator + currentValue, 0);
+
+
+
+
+
+
+
+
+
+
