798af3497fb89853e4cfc6e8f1857492c9b900d9
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-10 20:14:54 +0500

    responsive unit

---
+++ g4/2dars/index.js
@@ -0,0 +1,18 @@
+function getFirstValue(arr) {
+    return arr[0]
+}
+// console.log(getFirstValue([99, 100, 20]));
+
+function makePair(num1,num2) {
+
+}
+
+// console.log(makePair(213456, 2));
+
+
+
+// const makePair = (a, b) => [a, b];
+
+function parseArray(arr) {
+    return arr.join('').split('')
+}
