9778aa514da23613b91fd10cfc11d7a55ae72ddf
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-11 22:46:41 +0500

    projectNavbar

--- g5/3 dars arrey /index.js
+++ g5/3 dars arrey /index.js
@@ -1,12 +1,83 @@
+
 const one = [1, 2, 3, 4, 5, 6]
 
 // for (let i = 0; i < one.length; i++) {
 //     console.log(one[i]);
 // }
 
-for (value of one) {
-    // console.log(value);
-}
-for (index in one) {
-    console.log(index);
-}
+
+// for (const value in one) {
+//     console.log(value);
+// }
+// for (value of one) {
+//     console.log(value);
+// }
+// for (index in one) {
+//     // console.log(index);
+// }
+
+// var yourArr = [1, 2, 3]
+// [b, a] = yourArr
+
+// console.log(one);
+
+// console.log('foreach', exampleArray.forEach(x => console.log(x * x * x, 'forech')));
+//Output: (undefined)
+// console.log('----------');
+// console.log('map', exampleArray.map(x => x * x * x));
+//Output: [1 , 8, 27, 64, 125 ]
+
+// const obj = [
+//     {
+//         name: 'xasan',
+//         age: 21,
+//         status: 'Dasturchi',
+//         emai: 'xasan100@mail.ru',
+//         hobbe: 'uxlash',
+//     }
+//     ,
+//     {
+//         name: 'xusan',
+//         age: 22,
+//         status: 'Trading',
+//         emai: 'xusan100@mail.ru',
+//         hobbe: 'sayir',
+//     }
+//     , {
+//         name: 'sardor',
+//         age: 29,
+//         status: '',
+//         emai: 'sardor100@mail.ru',
+//         hobbe: 'futbol',
+//     }
+// ]
+// b = obj.map((value) => value.age)
+
+// function name(str) {
+//     return str.match(/[0-9]/g).join('')
+// }
+// console.log(name('She 3a24Walks345 Tao The345 Beach'));
+
+// console.log(b,'b');
+
+const a = [1, 2, 0, 1, 34, 12, 1]
+// b = a.reduce((a, b) => a + b, 0)
+// b = a.sort((a,b) => b-a)
+
+const arr = [
+    {
+        name: 'xasan',
+        id: 1,
+        status: 'students',
+        hobbe: 'sayir'
+    },
+
+    {
+        name: 'aziz',
+        id: 2,
+        status: 'devoloper',
+        hobbe: 'sayir'
+    }
+]
+
+console.log(this, 'this');
