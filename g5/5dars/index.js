dc3cb8559ab8aef0609d90cba52b4b24adc24ab5
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-19 16:05:21 +0500

    s

---
+++ g5/5dars/index.js
@@ -0,0 +1,38 @@
+const obj = {}
+obj.name = 'xasan'
+
+
+
+const map = new Map()
+map.set('title', 'xasan')
+map.set('title', 'xusan')
+map.set('title', 'fotima')
+map.set('title', 'zuhra')
+
+// map.forEach((value) => {
+//     console.log(value)
+// })
+// console.log(Object.fromEntries(map).title, 'map');
+
+// let gen = map.entries()
+// gen.next()
+// console.log(gen.next());
+
+
+
+// console.log(obj, 'obj');
+// console.log(map.delete('aziz'), 'map');
+// console.log(map.clear(), 'map');
+// console.log(map, 'map');
+
+
+
+
+
+const diyora = new Set()
+diyora.add('xasan')
+diyora.add('df')
+
+
+
+console.log(diyora.has(),'s');
