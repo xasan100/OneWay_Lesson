815be9baa3b17630fb306f6eb3927c0f0db79c57
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-20 15:59:37 +0500

    done

--- g4/2dars/index.js
+++ g4/2dars/index.js
@@ -20,4 +20,48 @@ const a = 1234567
 // console.log(`${resA}  ${resB} ${resC} ${resD}`);
 
 
-console.log(`${true + false}${false + false}${false + false}`);
+// console.log(`${true + false}${false + false}${false + false}`);
+let temp = 13
+// 1 ? console.log('havo sovuq') : 0
+// if (temp <= 15) {
+//     console.log('havo sovuq');
+// }
+// else if (temp < 30) {
+//     console.log( 'havo iliq');
+// }
+// else {
+//     console.log(`ma'lumot topilmadi`);
+// }
+
+
+let day = 4;
+let dayName;
+
+switch (day) {
+    case 1:
+        dayName = 'Sunday';
+        break;
+    case 2:
+        dayName = 'Monday';
+        break;
+    case 3:
+        dayName = 'Tuesday';
+        break;
+    case 4:
+        dayName = 'Wednesday';
+        break;
+    case 5:
+        dayName = 'Thursday';
+        break;
+    case 6:
+        dayName = 'Friday';
+        break;
+    case 7:
+        dayName = 'Saturday';
+        break;
+    default:
+        dayName = 'Invalid day';
+}
+// console.log(dayName);
+
+
