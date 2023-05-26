fab801052df84ddb79760b3e00a7cc89d84296c3
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-05-26 17:23:10 +0500

    css

--- g4/3dars/index.js
+++ g4/3dars/index.js
@@ -1,7 +1,7 @@
 // console.log(`${true+false}${false+false}${false+false}`);
 
 
-var one = 1 ? console.log('judda ham issiq') : console.log('malumot topilmadi') ? console.log('hey guys') : console.log('topilmaddi');
+// var one = 1 ? console.log('judda ham issiq') : console.log('malumot topilmadi') ? console.log('hey guys') : console.log('topilmaddi');
 // if (one < 25) {
 //     console.log('iliq havo');
 // }
@@ -16,3 +16,37 @@ var one = 1 ? console.log('judda ham issiq') : console.log('malumot topilmadi') 
 //     console.log('malumot topilmadi');
 // }
 
+
+
+let res = 7
+// switch (res) {
+//     case 1: console.log('dushanba');
+//         break
+//     case 2: console.log('seshanba');
+//         break
+//     case 3: console.log('chorshanba');
+//         break
+//     case 4: console.log('payshanba');
+//         break
+//     case 5: console.log('jumma');
+//         break
+//     case 6: console.log('shanba');
+//         break
+//     case 7: console.log('yakshanba');
+//         break
+//     default: console.log('malumot topilmadi');
+// }
+
+
+// for (let index = 0; index < array.length; index++) {
+
+
+// }
+res = 100
+for (i = 1; i <= res; i++) {
+    console.log(i);
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+
+}
