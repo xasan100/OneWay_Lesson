e9416fcdbef85e0621a9ef062ad5a102a935682f
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-06-03 10:46:56 +0500

    review

---
+++ g4/4dars/index.js
@@ -0,0 +1,72 @@
+// array = 'akmalbek'
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array.length; j++) {
+//         console.log(array[j], 'in');
+//     }
+//     console.log(array[i], 'out');
+// }
+
+
+
+
+
+// let i = 0;
+// let b = 'xasan'
+
+// while (i < b.length) {
+//     // console.log(i);
+//     i++
+// }
+
+// do {
+//     console.log('hey');
+// } while (0);
+
+
+const studendts = {
+    name: 'xasan',
+    status: 'students',
+    age: 18,
+    12: '998991006010'
+}
+
+let key = 'age'
+
+// console.log(studendts[age]);
+// console.log(studendts.key); 
+// console.log(studendts['key']);
+// console.log(studendts[key]);
+
+
+// const obj1 = { name: 'xasan' }
+// const obj2 = { name: 'xusan' }
+// obj3 = obj1
+
+// console.log(obj1 == obj3);
+
+let data = {
+    name: 'xasan',
+    age: 5,
+    chilid: {
+        name: 'asadbek',
+        age: 5,
+        chilid: {
+            age: 5,
+            name: 'xusan',
+            chilid: {
+                age: 10,
+                name: 'xursad'
+            }
+        }
+    },
+}
+
+let sum = 0
+// while (1) {
+//     if (data) sum += data.age
+//     else break
+//     data = data.chilid
+// }
+// console.log(sum);
+
+
