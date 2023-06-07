21f1ae7b199876f62639890442cf48955c885681
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-06-07 19:18:51 +0500

    varebil

---
+++ g1/java sctipt/index.js
@@ -0,0 +1,37 @@
+
+
+// data type
+// vareble O'zgaruvchi
+
+//1string 2number 3boolean  4undefined 5null 6arrey [{}] 7sybool
+// !@#$%^&*()(*&^%$#) 8Bigint
+
+// primativ 1string 2number 3boolean  4undefined 5null 7sybool 8Bigint
+// nan primative 6arrey [{ true, fuction }]
+// =+/ * ** Number.isNan()
+
+
+const numObj = 1234.30123;
+
+// console.log(numObj.toFixed(1));
+
+
+
+1
+// let a = 10
+// console.log(a*4);
+2
+// let b = 10
+// console.log(b * 4 / 2);
+3
+// let a = 5
+// let b = 5
+// s = a * b
+
+// p = 2 * (a + b)
+// console.log(b, s);
+4
+// d = 10
+// const p = 3.14
+// l = d * p
+// console.log(l);
