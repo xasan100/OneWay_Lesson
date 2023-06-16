f32e8f900fc67683d84948843b2da16f6a40fa17
Abdurazakov xasan <102683894+xasan100@users.noreply.github.com>
2023-06-16 21:26:16 +0500

    localStorage

--- g4/6dars/return.js
+++ g4/6dars/return.js
@@ -4,9 +4,7 @@
 // setTimeout(() => {
 //     console.log("this is the second message");
 // }, 3000);
-// setTimeout(() => {
-//     console.log("this is the third message");
-// }, 1000);
+
 
 let sum = 0
 const Count = (n) => {
@@ -45,68 +43,64 @@ Count(10)
 // };
 
 
+// setTimeout(() => {
+//     console.log("finshed....");
+// }, 0);
 
+// setTimeout(() => {
+//     console.log("loading....");
+// }, 1000);
 
-
+// console.log('started....');
 
 
 const res = '{"name":"asadbek", "age":42}';
+
 try {
-    // console.log(res, 'json');
-    // console.log(JSON.parse(res, 'jsondan chiqarilgani'));
-    // console.log(JSON.parse(json));
+    // console.log(JSON.parse(res));
 }
-catch {
-    console.log('bug');
+catch (error) {
+    console.log(error.name);
 }
 
 
-const dataBase = {
-    asadbek: {
-        login: 'asadbek',
-        password: 1234,
-        fullName: 'Asadbe Ubaydellayev'
-    }
-}
 
-console.log('started....');
-const loginF = (lg, ps, call) => {
-    setTimeout(() => {
-        if (dataBase.asadbek.login === lg && dataBase.asadbek.password === ps) {
-            return call(`Welcom to facebook ${dataBase.asadbek.fullName}`)
-        }
-        else {
-            return call('login yoki password hatolik bor')
-            
-                
-        }
-    }, 1000)
-}
- loginF('asadbek', 12345, (usr) => {
-    console.log(usr);
-})
-console.log('finished');
-
-
-
-
-
-// console.log('started');
-// const login = (lg, ps, call) => {
+// const loginFacebook = (lg, ps, call) => {
 //     setTimeout(() => {
-//         if (dataBase.xasan.login === lg && dataBase.xasan.password == ps) {
-//             return call(`Welcome To ${dataBase.xasan.fullName} `);
+//         if (dataBase.muhammad.login === lg && dataBase.muhammad.password === ps) {
+//             return call(`Welcome to ${dataBase.muhammad.fullName} Facebook`);
 //         }
 //         else {
-//             return `Malumot Notug'ri Kiritilgan`;
+//             return call('login yoki password xatolik bor soory !!!');
 //         }
-//     }, '1000')
+//     }, 1000);
 // }
-// login('xasan', 1234, (usr) => {
-//     console.log(usr);
-//     console.log('finished');
 
+// loginFacebook('muhammadali', 123434, (usr) => {
+//     console.log(usr);
+// });
+
+// console.log('started....');
+// const loginF = (lg, ps, call) => {
+//     setTimeout(() => {
+//         if (dataBase.asadbek.login === lg && dataBase.asadbek.password === ps) {
+//             return call(`Welcom to facebook ${dataBase.asadbek.fullName}`)
+//         }
+//         else {
+//             return call('login yoki password hatolik bor')
+
+
+//         }
+//     }, 1000)
+// }
+// loginF('asadbek', 12345, (usr) => {
+//     console.log(usr);
 // })
+// console.log('finished');
+
+
+
+
 
 
 
@@ -115,3 +109,63 @@ console.log('finished');
 // }, 2000);
 // console.log('started...');
 
+// const funck = (str) => {
+//     // b = str.join('').split('').filter((value) => value.toLowerCase() === 'a')
+//     // return b.length
+
+//     return str.join('').match(/[a-z]/gi)
+// }
+
+// console.log(funck(['xas043An', 'muhamm43ad Ali', 'Er4354kinJAnov']));
+
+
+
+
+// console.log('started');
+// const login = (lg, ps, call) => {
+//     setTimeout(() => {
+//         if (dataBase.muhammad.login === lg && dataBase.muhammad.password == ps) {
+//             return call(`Welcome To ${dataBase.muhammad.fullName} `);
+//         }
+//         else {
+//             return `Malumot Notug'ri Kiritilgan`;
+//         }
+//     }, '1000')
+// }
+// login('muhammadali', 1234, (usr) => {
+//     console.log(usr);
+//     console.log('finished');
+
+// })
+
+// const dataBase = {
+//     muhammad: {
+//         login: 'muhammadali',
+//         password: 1234,
+//         fullName: 'Muhammad saikov'
+//     }
+// }
+// console.log('started');
+// const myPromise = new Promise((resolve, reject) => {
+//     if (dataBase.muhammad.login === 'muhammadali' && dataBase.muhammad.password == 1234) {
+//         resolve(`Welcome To ${dataBase.muhammad.fullName} `);
+//     }
+//     else {
+//         reject(`Malumot Notug'ri Kiritilgan`);
+//     }
+// });
+// myPromise
+//     .then((result) => {
+//         console.log(result);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     });
+// console.log('fineshed');
+
+
+// localStorage.setItem('name', 'xasan')
+// localStorage.setItem('as', 'xasan')
+// localStorage.getItem('name')
+// localStorage.removeItem('as')
+// localStorage.clear()
